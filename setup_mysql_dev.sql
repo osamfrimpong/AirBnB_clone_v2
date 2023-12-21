328506a278e8bff38b7c70d2b476ae6c3c0fdfc6
Schandorf Osam-Frimpong <osamfrimpong@yahoo.com>
2023-12-21 05:12:58 +0000

    Added codes

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,5 +1,6 @@
--- sqldb will create a database with a new db
+-- sql code that will create a database with a new db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
